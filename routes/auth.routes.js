a522ee95d232ad5b0c866623de4a77d61089dc09
Dev deRev <devderev@free.fr>
2022-03-24 19:59:57 +0100

    iteration 2 Done

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -6,10 +6,62 @@ const bcrypt = require('bcrypt');
 // const saltRounds = bcrypt.getRounds(10)
 const saltRounds = 10;
 
-const session = require('express-session');
-
+// Just go to the auth page
 router.get('/', (req, res) =>{
     res.render('auth')
+})
+
+// Loggin from auth with form POST
+router.post('/', (req, res, next)=>{
+    //attemptedUrl was ignitiated in middleware is LoggedIn, and be injeted in form
+    const { username, password, attemptedUrl} = req.body;
+    // verify username
+    if(!username){
+        return res.status(400).render('auth',{
+            errorMessage: 'Please provide your username'
+        })
+    }
+    // verify password
+    if(password.length < 8){
+        return res.status(400).render('auth',{
+            errorMessage: 'Please provide a password longer than 7 characters'
+        })
+    }
+
+    // Then we search user in the db
+    User.findOne({username})
+    .then((user)=>{
+        // is username exist in db ?
+        if(!user){
+            console.log('not the same user');
+
+
+            return res.status(400).render('auth',{
+                errorMessage: 'Wrong Credentials, please check your Username or password'
+            })
+        }
+
+        // is pprovides password matches the one in db ? need bcrypt
+
+        bcrypt.compare(password, user.password)
+        .then((isSamePassword) => {
+            if (!isSamePassword){
+
+                console.log('not the same password');
+                return res.status(400).render('auth',{
+                    errorMessage: 'Wrong Credentials, please check your Username or password'
+                })
+            }
+            req.session.user = user 
+            return res.redirect(attemptedUrl ||'/')
+        })
+    })
+    .catch((err) => {
+        // the middleware gonna handle the err, with next()
+        console.error(err, 'ERRERUUUUUR')
+        next(err)
+    })
+ 
 })
 
 
@@ -28,13 +80,26 @@ router.post('/create', async (req, res) => {
             res.send('USER already exist !')
 
         }else if (!isUser){
-            // pour l'instant le MDP n'est pas hashé
-           const createdUser = await User.create({username, password}).then( (newUser) => {
-               console.log(newUser, "a été créé"),
-                req.session.user = newUser;
-
-               res.redirect('../routes/index.js')
-           })
+            
+            return bcrypt
+            .genSalt(saltRounds)
+            .then((salt) => {
+                // Je return le hashedPassword pour la suite .then
+                return bcrypt.hash(password, salt)})
+            .then((hashedPassword)=>{
+                // Now i create user
+                console.log(hashedPassword, 'MOT DE PASSS RETOUR');
+                return User.create({username,password: hashedPassword}).then( (newUser) => {
+                    console.log(newUser, "a été créé"),
+                     req.session.user = newUser;
+     
+                    res.redirect('/')
+                })
+            })
+            .catch( (error) => {
+                next(error)
+            })
+        
         }
     })
 })
